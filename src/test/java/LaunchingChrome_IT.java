2b5839bda3c65b2a1f176cf809e98104a28ea0b2
evgeniyamaslennikova <jen-o@yandex.ru>
2020-08-17 09:48:54 +0300

    Remove:
    - extra steps

--- src/test/java/LaunchingChrome_IT.java
+++ src/test/java/LaunchingChrome_IT.java
@@ -1,5 +1,4 @@
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,16 +10,11 @@ public class LaunchingChrome_IT {
     public void registration() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
-        WebElement elementFirstName = driver.findElement(By.id("user-name"));
-        elementFirstName.click();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        WebElement elementPassword = driver.findElement(By.id("password"));
-        elementPassword.click();
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         WebElement elementSubmit = driver.findElement(By.id("login-button"));
         elementSubmit.click();
         Assert.assertTrue(driver.getPageSource().contains("Product"));
-        System.out.println("Text is present");
         driver.close();
     }
 }
